6a404663aaf80decc6ab80357096a23f79e98cfe
Ritchie Bob B. Napinas <napinasritchiebob@gmail.com>
2025-05-05 17:18:30 +0800

    Track new files, commit modified ones, and handle deletions

--- node.js
+++ node.js
@@ -1,4 +1,3 @@
-// Toggle the menu visibility when the hamburger icon is clicked
 document.getElementById('menu-toggle').addEventListener('click', function() {
     const ul = document.querySelector('ul');
     ul.classList.toggle('show');
--- nodes.js
+++
@@ -1,5 +0,0 @@
-// Toggle the menu visibility when the hamburger icon is clicked
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const ul = document.querySelector('ul');
-    ul.classList.toggle('show');
-});
